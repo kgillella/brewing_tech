0d0a75a69d22bcc28a35d03b6f3db2799e412119
Hardikar <vhardikar@deloitte.com>
2018-02-01 16:23:56 +0530

    Changes to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import '../style/modules/App.css';
 class Header extends Component {
 render() {
   return (
-    <div className="Header">
-      <div className="left-header-item">
+    <div className="header">
+      <div className="leftHeaderItem">
       <img src="brewing.svg" alt="brewing-tech" height="42" width="42"/>
-      <select>
+      <select className="selectValues">
         <option value="Yearly">Yearly</option>
         <option value="Monthly">Monthly</option>        
       </select>

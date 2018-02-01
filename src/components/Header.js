090b1922c440832f442b63c0e140368128fc80d2
Hardikar <vhardikar@deloitte.com>
2018-02-01 16:10:43 +0530

    Added Header image and dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import '../style/modules/App.css';
 
 class Header extends Component {
-  render() {
-    return (
-      <div className="Header">
-        {/*Please add the HTML code*/}
+render() {
+  return (
+    <div className="Header">
+      <div className="left-header-item">
+      <img src="brewing.svg" alt="brewing-tech" height="42" width="42"/>
+      <select>
+        <option value="Yearly">Yearly</option>
+        <option value="Monthly">Monthly</option>        
+      </select>
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 export default Header;

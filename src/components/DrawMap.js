b0d037f06a10187c3fc5e51387cedaacd5cc2b68
Kamath <sahkamath@deloitte.com>
2018-02-01 10:47:38 +0530

    fetching data from API

--- src/components/DrawMap.js
+++ src/components/DrawMap.js
@@ -3,6 +3,20 @@ import GoogleMapReact from 'google-map-react';
 import '../style/modules/App.css';
 
   class DrawMap extends React.Component {
+
+    constructor(props) {
+      super(props)
+      this.state = { 
+        data:''
+      }
+    }
+
+    componentDidMount(){
+      fetch('https://1pssztfj82.execute-api.us-east-1.amazonaws.com/dev/slotsinfo/get').then(function (data){
+        console.log(data);
+      })
+    }
+  
     static defaultProps = {
       center: {lat: 12.9442212, lng:77.6936836},
       zoom: 11
